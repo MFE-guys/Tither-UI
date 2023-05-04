172702d77ea079e2eb30719189a7a6f79a4d769a
Josileudo Rodrigues <josileudorodrigues75@gmail.com>
2023-05-04 18:38:52 -0300

    :lipstick: style: Dark and Light theme created

--- src/app/components/Header/index.ts
+++ src/app/components/Header/index.ts
@@ -1,16 +1,56 @@
-import { Component } from '@angular/core';
+import {AfterContentInit, ChangeDetectionStrategy, Component} from '@angular/core';
+import {FormsModule} from "@angular/forms";
+import {NgClass} from "@angular/common";
+import {ButtonModule} from "primeng/button";
+import {ThemeService} from "../../theme.service";
+
+export class ThemeConfig {
+  icon: string;
+  theme: string;
+
+  constructor() {
+    this.icon = 'pi pi-moon';
+    this.theme = 'saga-green';
+  }
+}
 
 @Component({
   selector: 'app-header',
   standalone: true,
+  imports: [FormsModule, ButtonModule, NgClass],
   template: `
-    <header class="bg-green-700">
-      <div class="max-w-8xl mx-auto">
-        <div class="flex flex-row items-center py-4 px-4">
-          <span class="text-white font-bold">TITHER</span>
+    <header>
+      <div class="w-60rem m-auto flex justify-content-between align-items-center py-4 px-4">
+        <div class="flex flex-row items-center">
+          <span class="font-bold">TITHER</span>
+        </div>
+
+        <div class="flex justify-content-between align-items-center gap-2">
+          <p-button
+            [icon] = 'theme.icon'
+            styleClass="p-button-rounded"
+            (click)="changeTheme()"
+          />
         </div>
       </div>
     </header>
-  `
+
+
+  `,
+  styles: [` `]
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  check?: boolean;
+  theme = new ThemeConfig()
+  constructor(private themeService: ThemeService) {}
+
+  changeTheme() {
+    this.theme = new ThemeConfig();
+    this.check =! this.check
+
+    if(this.check) this.theme = { theme: 'vela-green', icon: 'pi pi-sun' }
+    else this.theme = { theme: 'saga-green', icon: 'pi pi-moon' }
+
+    this.themeService.switchTheme(this.theme.theme);
+  }
+}
