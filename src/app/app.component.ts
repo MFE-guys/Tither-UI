c0f15c3ca3547b5422fa73b1fbb1ff9841992b58
Josileudo Rodrigues <josileudorodrigues75@gmail.com>
2023-05-04 18:38:52 -0300

    :lipstick: style: Dark and Light theme created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,17 +3,25 @@ import { Component, OnInit } from '@angular/core';
 import { PrimeNGConfig } from 'primeng/api';
 
 import { HeaderComponent } from './components/Header';
+import {ThemeService} from "./theme.service";
+import {ButtonModule} from "primeng/button";
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, ButtonModule],
   standalone: true,
-  template: `<app-header />`
+  template: `
+    <app-header />
+  `
 })
 export class AppComponent implements OnInit {
-  constructor(private primengConfig: PrimeNGConfig) {}
+  constructor(
+    private primengConfig: PrimeNGConfig,
+    private themeService: ThemeService
+  ) {}
 
   ngOnInit(): void {
     this.primengConfig.ripple = true;
   }
+
 }
