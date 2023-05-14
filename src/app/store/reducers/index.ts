15d1c42b07b2cd7adfce4ed4b349e4730273c941
josileudo <josileudorodrigues75@gmail.com>
2023-05-14 19:28:08 -0300

    :recycle: refactor: Set constants values

--- src/app/store/reducers/index.ts
+++ src/app/store/reducers/index.ts
@@ -1,17 +1,19 @@
 import { signal } from '@angular/core';
+import { Constants } from 'src/app/utils/constants';
 
 import { Action, createReducer, on } from '@ngrx/store';
 
 import { themeActions } from '../actions';
 
+const { dark, light } = Constants.theme;
 export const initialTheme = 'saga-green';
 export const keyFeature = 'theme';
 export const globalTheme = signal(initialTheme);
 
 const _themeReducer = createReducer(
   initialTheme,
-  on(themeActions.lightTheme, () => 'saga-green'),
-  on(themeActions.darkTheme, () => 'vela-green')
+  on(themeActions.lightTheme, () => light),
+  on(themeActions.darkTheme, () => dark)
 );
 
 export function themeReducer(
