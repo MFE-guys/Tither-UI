7df92763dbc86e7700333fc8f4b7f20f86c9a38d
josileudo <josileudorodrigues75@gmail.com>
2023-05-14 19:04:25 -0300

    :wrench: chore: Added Jest configs

---
+++ jest.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  preset: 'jest-preset-angular',
+  setupFilesAfterEnv: ['<rootDir>/src/setup.jest.ts'],
+  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/dist/'],
+  collectCoverage: true
+};
