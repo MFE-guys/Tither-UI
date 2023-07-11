2040ccc060eb415b0e16a47aecf5fb3b9216e109
josileudo <josileudorodrigues75@gmail.com>
2023-07-11 18:42:29 -0300

    :wrench: chore: Create effect to added member

--- src/app/store/effects/register-member.effect.ts
+++ src/app/store/effects/register-member.effect.ts
@@ -1,14 +1,40 @@
 import { Injectable, inject } from '@angular/core';
 import { RegisterMemberService } from 'src/app/core/services/register-member.service';
+import { RegisterMemberRequiredProps } from 'src/app/core/model/interface/register-member.interface';
 
-import { exhaustMap } from 'rxjs';
+import { catchError, exhaustMap, from, map, of, switchMap, tap } from 'rxjs';
 
 import { Actions, createEffect, ofType } from '@ngrx/effects';
 
-import { RegisteredMemberApiActions } from '../actions/register-member.actions';
+import {
+  RegisterMemberActions,
+  RegisteredMemberApiActions
+} from '../actions/register-member.actions';
+import { MessageActions } from '../actions/message.actions';
 
 @Injectable()
 export class RegisterMemberEffect {
   private actions$ = inject(Actions);
   private registerMemberService = inject(RegisterMemberService);
+
+  addMember$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(RegisterMemberActions.registerMemberAdded),
+      exhaustMap(({ register }) =>
+        from(this.registerMemberService.registerMember(register)).pipe(
+          map(response => response),
+          map((registerMember: RegisterMemberRequiredProps) =>
+            RegisteredMemberApiActions.registeredMemberAdded({
+              register: registerMember
+            })
+          ),
+          catchError(err =>
+            of(
+              RegisteredMemberApiActions.registeredMemberFailure({ error: err })
+            )
+          )
+        )
+      )
+    )
+  );
 }
